54f1da8e3ae85657256578af4a0e5718a790ff7d
adampryono <adampryono@gmail.com>
2026-02-12 09:29:34 +0700

    Update: Add Minion video, responsive design, and Replit config

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Kamu serius sayangg?",
+    "Yang benerrr :((??",
+    "Ihhh sayangg kenapa gituu...",
+    "Sayangg pleaseee...",
+    "Coba dipikir lagi dehh...",
+    "Kalo kamu gamau ak sedihh...",
+    "Sedihhhh bangeettt",
+    "Ayo dongg sayang...",
+    "Oke dehhh janji yang terakhir yaaa...",
+    "Bercanda sayangg, say yes pleaseee ❤️"
 ];
 
 let messageIndex = 0;
